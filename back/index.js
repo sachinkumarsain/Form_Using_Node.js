6812a5df6effe6e66f4023907dc0c04c158e0c66
sachin <sachinsain21082004@gmail.com>
2023-07-04 18:55:24 +0530

    mongodb data push

--- back/index.js
+++ back/index.js
@@ -1,14 +1,16 @@
 import cors from "cors"
 import express from "express"
-import connection from "../front/src/db"
-import user from "../front/src/models"
+import connection from "./db.js";
+import user from "./models.js";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "http://localhost:3000" }));
 
-app.get("/", (req, res) => { })
+app.get("/", (req, res) => { 
+    res
+})
 
 app.post("/register", async (req, res) => {
     const { name, email, phone, username, password } = req.body;
